bcac2d062dc455283d16488065177a86ec9331a0
andre <andrebeet1@gmail.com>
2025-05-11 09:54:47 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://swap-sim-app.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
